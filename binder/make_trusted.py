e06c79f3d4270bb907923c8e1513a8d49ce95d8e
IbH <Ib.Hansen.iv@gmail.com>
2020-09-17 10:13:32 +0200

    POSTBBUILD TRUSTED

--- binder/make_trusted.py
+++ binder/make_trusted.py
@@ -7,8 +7,13 @@ Created on Thu Sep 17 09:44:22 2020
 
 from pathlib import Path, PurePosixPath, PurePath
 
-for dir in Path('../').glob('**'):
-    if len(dir.parts) and str(dir.parts[-1]).startswith('.'): continue
-    for notebook in dir.glob('*.ipynb'):
-        n = PurePosixPath(notebook)
-        print(str(n).split('/',1)[1])
+with open('Postbuild','wt') as f:
+    f.write('''\
+#!/bin/bash 
+conda install modelflow -c ibh --y --no-deps''')
+    for dir in Path('../').glob('**'):
+        if len(dir.parts) and str(dir.parts[-1]).startswith('.'): continue
+        for notebook in dir.glob('*.ipynb'):
+            n = PurePosixPath(notebook)
+            f.write(f"jupyter trust {str(n).split('/',1)[1]}\n")
+    f.write('exec "$@"')
