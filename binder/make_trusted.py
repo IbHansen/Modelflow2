891e92f632becddd1a272eeb3f4a0400eed295e3
IbH <Ib.Hansen.iv@gmail.com>
2020-09-21 00:20:19 +0200

    works

--- binder/make_trusted.py
+++ binder/make_trusted.py
@@ -10,7 +10,7 @@ from pathlib import Path, PurePosixPath, PurePath
 with open('Postbuild','wt') as f:
     f.write('''\
 #!/bin/bash 
-conda install modelflow -c ibh --y --no-deps
+conda install modelflow -c ibh --y  #  --no-deps
 ''')
     for dir in Path('../').glob('**'):
         if len(dir.parts) and str(dir.parts[-1]).startswith('.'): continue
