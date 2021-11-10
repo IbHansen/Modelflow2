6747b91a681d0a7ea926dde969e5c25faa848f15
IbHansen <43575358+IbHansen@users.noreply.github.com>
2021-11-10 14:09:29 +0100

    Create model_run_numba.py

---
+++ modelflow/model_run_numba.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+This script runs a model with numba  
+
+
+@author: hanseni 
+"""
+
+import sys
+from modelclass import model 
+
+mmodel,   basedf    = model.modelload(sys.argv[1],run=1,ljit=1,stringjit=False)
