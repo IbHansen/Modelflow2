e8f72efd555c72f58f721c5f0a9692d8cbd7e16b
IbHansen <43575358+IbHansen@users.noreply.github.com>
2026-03-24 17:25:38 +0100

    Update modelmf.py

--- modelflow/modelmf.py
+++ modelflow/modelmf.py
@@ -9,121 +9,16 @@ Created on Sat March 2019
 """
 
 import pandas as pd
-from collections import namedtuple
-import inspect 
 import fnmatch
 
 
 
 from modelclass import model
-import modelvis as mv
-from modelmanipulation import check_syntax 
+from modelhelp import debug_var
 
 
-if not hasattr(pd.DataFrame,'mf'):
-    @pd.api.extensions.register_dataframe_accessor("mf")
-    class mf():
-        '''A class to extend Pandas Dataframes with ModelFlow functionalities
+if not hasattr(pd.DataFrame,'mfcalc'):
         
-        Not to be used on its own 
-        
-        '''
-        
-        def __init__(self, pandas_obj):
-    #        self._validate(pandas_obj)
-            self._obj = pandas_obj
-            self.modelopt = {}
-            self.solveopt = {}
-            self.eq = ''
-    #        print(self._obj)
-            
-        def _setmodel(self,m):
-            ''' a function to be prepared'''
-            self.model = m
-      
-        def copy(self):
-            ''' copy a modelflow extended dataframe, so it remember its model and options'''
-            
-            res = self._obj.copy()
-            
-            res.mf._setmodel(self.model)
-            res.mf.modelopt = self.modelopt
-            res.mf.solveopt = self.solveopt 
-            res.mf.eq = self.eq 
-            return res
-        
-        def solve(self,start='',end='',**kwargs):
-            '''Solves a model'''
-            this = self._obj
-    #        print(kwargs)
-            self.solveopt = {**self.solveopt,**kwargs,**{'start':start,'end':end}} 
-    #        print(self.solveopt)
-    #        print(self.solveopt)
-            res = self.model(this,**self.solveopt)
-            res.mf._setmodel(self.model)
-            res.mf.modelopt = self.modelopt
-            res.mf.solveopt = self.solveopt 
-            return res 
-                
-        def makemodel(self,eq,**kwargs):
-            '''Makes a model from equations'''
-            # self.eq = eq
-            self.eq = "\n".join(line.lstrip() for line in eq.splitlines())
-            # check_syntax(self.eq)
-            self.modelopt = {**self.modelopt,**kwargs} 
-            
-            self.model = model.from_eq(self.eq,**kwargs)
-            return self.copy()
-        
-        def __call__(self,eq='',**kwargs):
-            '''
-            returns a model instace 
-
-            Args:
-                eq (TYPE, optional): DESCRIPTION. Defaults to ''.
-                **kwargs (TYPE): DESCRIPTION.
-
-            Returns:
-                model: a instance .
-
-            '''
-            if self.eq is eq:
-                return self
-            else:
-                res = self.makemodel(eq,**kwargs)
-                return res.mf
-        
-        
-        def __getattr__(self, name):
-            try:
-    #            print('dddddd',name)
-                return self.model.__getattribute__(name)
-            except:
-                pass
-            
-            try:
-    #            print('dddddd',name)
-                return self.model.__getattribute__(name.upper())
-            except:
-                pass
-            
-            try:
-                return mv.varvis(self.model,name.upper())
-            except: 
-                print(f'Error calling this attribute: {name}')
-                raise AttributeError 
-                pass   
-                 
-        def __getitem__(self, name):
-            '''Retrieves  __getitem__ from model instance'''
-            a=self.model.__getitem__(name)
-            return a
-        
-        def __dir__(self):
-            ''' to help tab-completion '''
-            res = list(self.__dict__.keys())
-            return res
-    
     @pd.api.extensions.register_dataframe_accessor("mfcalc")
     class mfcalc():
         '''
@@ -162,30 +57,46 @@ if not hasattr(pd.DataFrame,'mf'):
             '''
             
             # print({**kwargs,**{'start':start,'end':end}})
-            if (l0:=eq.strip()).startswith('<'):
-                timesplit = l0.split('>',1)
-                time_options = timesplit[0].replace('<','').replace(',',' ').replace(':',' ').replace('/',' ').split()
-                
-                if len(time_options)==1:
+            if (l0 := eq.strip()).startswith('<'):
+                timesplit = l0.split('>', 1)
+                if len(timesplit) != 2:
+                    raise Exception(f'Malformed time specification\nOffending: "{l0}"')
+            
+                time_options = timesplit[0].replace('<', '').replace(',', ' ').replace(':', ' ').replace('/', ' ').split()
+            
+                if len(time_options) == 1:
                     start = time_options[0]
                     end = time_options[0]
-                elif len(time_options)==2:
-                    start,end = time_options
-                else:     
-                    raise Exception(f'To many times \nOffending:"{l0}"')
+                elif len(time_options) == 2:
+                    start, end = time_options
+                else:
+                    raise Exception(f'Too many times\nOffending: "{l0}"')
+            
                 xeq = timesplit[1]
                 blanksmpl = False
-            else: 
-                xeq=eq
-                blanksmpl = True 
+            
+                offending = [line.strip() for line in xeq.splitlines() if line.strip().startswith('<')]
+                if offending:
+                    offending_text = "\n".join(offending)
+                    raise Exception(
+                        'More than one time specification is not allowed\n'
+                        'Offending lines:\n'
+                        f'{offending_text}'
+                    )
+            else:
+                xeq = eq
+                blanksmpl = True            
+            
+            mmodel = model.from_eq(xeq,modelname='MFCalc', **kwargs)
 
-            res = self._obj.mf(xeq,**kwargs).solve(**{**kwargs,**{'start':start,'end':end,'silent':True}})
+            res = mmodel(self._obj,**{**{'silent':True},**kwargs,**{'start':start,'end':end,}})
+
             # print('jddd')
             if blanksmpl:  
-                if self._obj.mf.model.maxlag or self._obj.mf.model.maxlead: 
-                    print(f'* Take care. Lags or leads in the equations, mfcalc run for {self._obj.mf.model.current_per[0]} to {self._obj.mf.model.current_per[-1]}')
+                if mmodel.maxlag or mmodel.maxlead: 
+                    print(f'* Take care. Lags or leads in the equations, mfcalc run for {mmodel.current_per[0]} to {mmodel.current_per[-1]}')
             if showeq: 
-                print(self._obj.mf.equations)
+                print(mmodel.equations)
             return res
         
     @pd.api.extensions.register_dataframe_accessor("mfupdate")
@@ -283,8 +194,6 @@ def f(a):
 
 if __name__ == '__main__':
     #this is for testing 
-        df = pd.DataFrame({'Z': [1., 2., 3,4] , 'TY':[10.,20.,30.,40.] ,'YD':[10.,20.,30.,40.]},index=   [2017,2018,2019,2020])
-        df1 = pd.DataFrame({'Z': [1., 2., 3,4] , 'TY':[10.,20.,30.,40.] ,'YD':[10.,20.,30.,40.]},index=   [2017,2018,2019,2020])
         df2 = pd.DataFrame({'Z':[1., 22., 33,43] , 'TY':[10.,20.,30.,40.] ,'YD':[10.,20.,30.,40.]},index=[2017,2018,2019,2020])
         df3 = pd.DataFrame({'Z':[1., 22., 33,43] , 'TY':[10.,20.,30.,40.] ,'YD':[10.,20.,30.,40.]},index=[2017,2018,2019,2020])
         df4 = pd.DataFrame({'Z':[ 223., 333] , 'TY':[203.,303.] },index=[2018,2019])
@@ -298,21 +207,8 @@ if __name__ == '__main__':
         dogplace = y *4 '''
         assert 1==1
             #%%
-        d22=(m2:=model.from_eq(ftest,funks=[f]))(df4)
-        aa = df.mf(ftest,funks=[f]).solve()
-        aa.mf.solve()
-        aa.mf.drawmodel() 
-        aa.mf.drawendo() 
-        # aa.mf.YX.draw()
-        aa.mf.istopo
-        aa.mf['*']
         df2.mfcalc(ftest,funks=[f],showeq=1)
-        df2.mf.Y.draw(all=True,pdf=0)
-        #%%
         df4 = pd.DataFrame({'Z':[ 223., 333] , 'YD':[203.,303.] },index=[2018,2019])            
         x = df2.mfupdate(df4)
         #%% test graph 
-        import networkx as nx
-        zz = df2.mf.totgraph
-        nx.draw(zz)
         
